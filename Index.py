2e7220971b72fbd42489f6d51b24ddaba80f9024
Kevin Nelson <kpie314@gmail.com>
2016-03-25 21:26:39 -0700

    fix file delims

--- Index.py
+++ Index.py
@@ -9,40 +9,34 @@ class indexer:
 	
 	def __init__(self,p):
 		self.path=p
-		
-	def HtmlFrek(self,k):
-		print("rek")
-		os.chdir(k)
-		self.loc.append(k)
-		ret="<h2>"+k+"</h2>"
+	def fprep(self,name):
+		name.replace(".","")
+		name.replace("\\","/")
+		return(name)
+	def refPrep(self):
+		ref = self.site+"/"+self.proj
+		for qw in self.loc:
+			ref+="/"+qw
+		return(ref)
+	def HtmlFrek(self,adir):
+		self.loc.append(adir)
+		os.chdir(adir)
+		ret="<h2>"+adir+"</h2>"
 		files = [f for f in os.listdir('.') if os.path.isfile(f) and f.split(".")[len(f.split("."))-1]=="html"]
 		for t in files:
-			print t
-			t.replace(".","")
-			t.replace("\\","/")
-			ref = self.site+"/"+self.proj
-			for k in self.loc:
-				ref+="/"+k
-			ret+= "<a href ="+ref+t+">"+t+"</a><br>\n"
+			ret+="<a href ="+self.refPrep()+"/"+self.fprep(t)+">"+self.fprep(t)+"</a><br>\n"
 		images = [f for f in os.listdir('.') if os.path.isfile(f) and f.split(".")[len(f.split("."))-1]=="png"]
 		for i in images:
-			i.replace(".","")
-			i.replace("\\","/")
-			ref = self.site+"/"+self.proj
-			for k in self.loc:
-				ref+="/"+k
+			i = self.fprep(i)
+			ref = self.refPrep()
 			ret+= "<img src="+ref+"/"+i+">\n"
 		folders = [f for f in os.listdir(".") if not os.path.isfile(f)]
-		print folders
 		for k in folders:
-			print k
-			if(k == '.'):
-				print k
+			if(k.__contains__(".")):
 				continue
-			print k
 			ret+="<div class='blue1'>"
 			ret+=self.HtmlFrek(k)
-			ret += "</div>"
+			ret+="</div>"
 		os.chdir("..")
 		del self.loc[len(self.loc)-1]
 		return(ret)
@@ -54,13 +48,12 @@ class indexer:
 		ret+="<div>"
 		folders = [f for f in os.listdir(".") if not os.path.isfile(f)]
 		for k in folders:
-			if(k == '.'):
+			if(k.__contains__(".")):
 				continue
 			print k
 			ret+="<div>"
 			ret+=self.HtmlFrek(k)
 			ret+="</div>"
-			
 		ret+="</div>"
 		ret+="""</html>"""
 		self.prod = ret
@@ -68,7 +61,7 @@ class indexer:
 		
 i = indexer(".")
 q=i.HtmlProd()
-print i.prod
+#print i.prod
 
 w = open("index.html","w")
 w.write(q)
