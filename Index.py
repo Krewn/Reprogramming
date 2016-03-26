1f694bc0ec4559255d305256434b6abdb60343ed
Kevin Nelson <kpie314@gmail.com>
2016-03-25 20:42:14 -0700

    Fix Index

--- Index.py
+++ Index.py
@@ -3,29 +3,33 @@ import os
 class indexer:
 	path = "~"
 	site = "http://krewn.github.io"
-	proj = "datasci"
+	proj = "Reprogramming"
 	prod = []
+	loc=[]
+	
 	def __init__(self,p):
 		self.path=p
+		
 	def HtmlFrek(self,k):
 		print("rek")
 		os.chdir(k)
+		self.loc.append(k)
 		ret="<h2>"+k+"</h2>"
 		files = [f for f in os.listdir('.') if os.path.isfile(f) and f.split(".")[len(f.split("."))-1]=="html"]
 		for t in files:
 			print t
 			t.replace(".","")
 			t.replace("\\","/")
-			ref = site+"/"+proj
-			for k in loc:
+			ref = self.site+"/"+self.proj
+			for k in self.loc:
 				ref+="/"+k
 			ret+= "<a href ="+ref+t+">"+t+"</a><br>\n"
 		images = [f for f in os.listdir('.') if os.path.isfile(f) and f.split(".")[len(f.split("."))-1]=="png"]
 		for i in images:
 			i.replace(".","")
 			i.replace("\\","/")
-			ref = site+"/"+proj
-			for k in loc:
+			ref = self.site+"/"+self.proj
+			for k in self.loc:
 				ref+="/"+k
 			ret+= "<img src="+ref+"/"+i+">\n"
 		folders = [f for f in os.listdir(".") if not os.path.isfile(f)]
@@ -40,10 +44,10 @@ class indexer:
 			ret+=self.HtmlFrek(k)
 			ret += "</div>"
 		os.chdir("..")
+		del self.loc[len(self.loc)-1]
 		return(ret)
 		
 	def HtmlProd(self):
-		location = []
 		print("start")
 		ret = ""
 		ret+="""<!DOCTYPE html><html>"""
